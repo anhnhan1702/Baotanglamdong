0720385639c8debb041a550a5ca6ddcb4fa1d618
anhnhan1702 <68462921+anhnhan1702@users.noreply.github.com>
2025-02-10 15:25:27 +0700

    thử xóa lỗi

--- app/Console/Commands/ImportFromDataBackupCommand.php
+++ app/Console/Commands/ImportFromDataBackupCommand.php
@@ -32,58 +32,6 @@ class ImportFromDataBackupCommand extends Command
      */
     public function handle()
     {
-        $dataBackups = \DB::table('databackups')->orderBy('id', 'asc')->get();
-        $username = config('database.connections.mysql.username');
-        $password = config('database.connections.mysql.password');
-        $host = config('database.connections.mysql.host');
-        $database = config('database.connections.mysql.database');
-
-        $month = date('m');
-        $year = date('Y');
-
-        $created_at = $updated_at = date('Y-m-d H:i:s');
-        $time = now()->timestamp;
-        $filename = "backup-" . $time . ".sql";
-        $backupPath = storage_path('app/backup').'/'.$year.'/'.$month.'/';
-        $filePath = "{$backupPath}/{$filename}";
         
-        // Tạo thư mục nếu chưa tồn tại
-        if (!file_exists($backupPath)) {
-            mkdir($backupPath, 0755, true);
-        }
-        $backupCommand = "mysqldump --user={$username} --password={$password} --host={$host} {$database} > {$filePath}";
-        $backupReturnVar = null;
-        $backupOutput = null;
-        exec($backupCommand, $backupOutput, $backupReturnVar);
-
-        if ($backupReturnVar !== 0) {
-            return; 
-        }
-
-        $path = $this->argument('path');
-        // Import file SQL
-        $command = "mysql --user={$username} --password={$password} --host={$host} {$database} < {$path}";
-
-        $returnVar = null;
-        $output = null;
-
-        exec($command, $output, $returnVar);
-        \DB::table('databackups')->truncate();
-        foreach($dataBackups as $dataBackup){
-            \DB::table('databackups')->insert([
-                'name' => $dataBackup->name,
-                'size' => $dataBackup->size,
-                'timebackup'  => $dataBackup->timebackup,
-                'created_at' => $dataBackup->created_at,
-                'created_at' => $dataBackup->created_at,
-            ]);
-        }
-        $id = \DB::table('databackups')->insertGetId([
-            'name' => $filename,
-            'size' => filesize($filePath),
-            'timebackup'  => $time,
-            'created_at' => $created_at,
-            'created_at' => $updated_at,
-        ]);
     }
 }
